3f0de880fd721ea9462642417bbb70ed561823c7
Francesco Molitierno <franc.molitierno@gmail.com>
2019-01-09 00:00:00 +0100

    firewall rule

--- remove.sh
+++ remove.sh
@@ -3,4 +3,4 @@
 opkg remove aria2 
 rm /usr/sbin/aria2-*
 rm /www/docroot/aria2
-
+sed -e '/#aria2/,+2d' /etc/firewall.user
--- setup.sh
+++ setup.sh
@@ -3,6 +3,11 @@
 opkg install ./*.ipk
 cp ./aria2-* /usr/sbin/
 cp ./aria2 /etc/init.d/
+cat <<EOF >> /etc/firewall.user
+#aria2
+iptables -I INPUT  -p tcp -m tcp --dport 6800:6801 -j ACCEPT
+iptables -I INPUT  -p udp -m udp --dport 6801 -j ACCEPT
+EOF
 cp ./remove.sh /usr/share/transformer/scripts/remove_aria2.sh
 if [ -d /mnt/usb/USB-A1 ]; then
     mkdir -p /mnt/usb/USB-A1/sharing/config/aria2
