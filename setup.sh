491466807694ad996beb14713ebefab740f803fe
Francesco Molitierno <franc.molitierno@gmail.com>
2019-01-09 00:44:46 +0100

    update install

--- setup.sh
+++ setup.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
 
-opkg install /mnt/usb/USB-A1/sharing/install/transmission/*.ipk
-cp /mnt/usb/USB-A1/sharing/install/transmission/transmission-st* /usr/sbin/
-cp /mnt/usb/USB-A1/sharing/install/transmission/transmission /etc/init.d/
-cp /mnt/usb/USB-A1/sharing/install/transmission/transmission.conf /etc/config/transmission
-cd /usr/share/ 
-ln -s /mnt/usb/USB-A1/sharing/config/transmission/
-cd /www/docroot/
-ln -s /mnt/usb/USB-A1/sharing/config/transmission/
-
+opkg install ./*.ipk
+cp ./transmission-st* /usr/sbin/
+cp ./transmission /etc/init.d/
+cp ./transmission.conf /etc/config/transmission
+#cat <<EOF >> /etc/firewall.user
+#iptables -I INPUT  -p tcp -m tcp --dport 6800:6801 -j ACCEPT
+#iptables -I INPUT  -p udp -m udp --dport 6801 -j ACCEPT
+#EOF
+cp ./remove.sh /usr/share/transformer/scripts/remove_transmission.sh
+if [ -d /mnt/usb/USB-A1 ]; then
+    mkdir -p /mnt/usb/USB-A1/sharing/config/transmission/
+    cp -R ./config/* /mnt/usb/USB-A1/sharing/config/transmission/
+    ln -s /mnt/usb/USB-A1/sharing/config/transmission/ /usr/share/transmission
+    ln -s /mnt/usb/USB-A1/sharing/config/transmission/web /www/docroot/transmission
+fi
