43a0b67c01487fb5f19442a4cf99e9ac56d79c16
Francesco Molitierno <franc.molitierno@gmail.com>
2019-01-08 23:45:47 +0100

    fix remove

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@
 opkg install ./*.ipk
 cp ./aria2-* /usr/sbin/
 cp ./aria2 /etc/init.d/
+cp ./remove.sh /usr/share/transformer/scripts/remove_aria2.sh
 if [ -d /mnt/usb/USB-A1 ]; then
     mkdir -p /mnt/usb/USB-A1/sharing/config/aria2
     cp -R ./config/* /mnt/usb/USB-A1/sharing/config/aria2
