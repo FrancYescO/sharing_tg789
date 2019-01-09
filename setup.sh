ea59c6552e0da2c72f0432526b9561904bd330b4
Francesco Molitierno <franc.molitierno@gmail.com>
2019-01-09 01:55:59 +0100

    create download dirs

--- setup.sh
+++ setup.sh
@@ -10,6 +10,8 @@ EOF
 cp ./remove.sh /usr/share/transformer/scripts/remove_aria2.sh
 if [ -d /mnt/usb/USB-A1 ]; then
     mkdir -p /mnt/usb/USB-A1/sharing/config/aria2
+    mkdir -p /mnt/usb/USB-A1/sharing/download/amule/files
+    mkdir -p /mnt/usb/USB-A1/sharing/download/amule/temp
     cp -R ./config/* /mnt/usb/USB-A1/sharing/config/aria2
     ln -s /mnt/usb/USB-A1/sharing/config/aria2/webui /www/docroot/aria
 fi
