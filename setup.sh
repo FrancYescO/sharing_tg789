cb65beeefc8589f44fbbdea36ef4850ba810418e
Francesco Molitierno <franc.molitierno@gmail.com>
2019-01-09 01:44:06 +0100

    remove old commands

--- setup.sh
+++ setup.sh
@@ -1,22 +1,5 @@
 #!/bin/sh
 
-opkg install /mnt/usb/USB-A1/sharing/install/amule/*.ipk
-cp /mnt/usb/USB-A1/sharing/install/amule/amule-* /usr/sbin/
-cp /mnt/usb/USB-A1/sharing/install/amule/amule /etc/init.d/
-rm -r /usr/share/amule/webserver
-cd /usr/share/amule/
-ln -s /mnt/usb/USB-A1/sharing/config/amule/webserver
-cd /www/docroot/
-ln -s /mnt/usb/USB-A1/sharing/config/amule/webserver
-
-# aMule
-iptables -I INPUT  -p tcp --dport 4662 -j ACCEPT
-iptables -I INPUT  -p udp --dport 4665 -j ACCEPT
-iptables -I INPUT  -p udp --dport 4672 -j ACCEPT
-
-
-#!/bin/sh
-
 opkg install ./*.ipk
 cp ./amule-* /usr/sbin/
 cp ./amule /etc/init.d/
