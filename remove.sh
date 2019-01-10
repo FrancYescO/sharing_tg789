715f64912f690cb12b11c3eb13090671ba58b38d
Francesco Molitierno <franc.molitierno@gmail.com>
2019-01-10 17:16:24 +0100

    move all dir to /tmp avoid replacing ipk files, putting synclink to usb folder to avoid writing lot o files on flash

--- remove.sh
+++ remove.sh
@@ -1,12 +1,6 @@
 #!/bin/sh
 
-rm /www/docroot/webserver
-#rm /usr/share/amule/webserver
-#mkdir /usr/share/amule/webserver
-#cp -pidRv /mnt/usb/USB-A1/sharing/config/amule/webserver/default /usr/share/amule/webserver/
 opkg remove amule libbfd libupnp libreadline libwxbase libncurses terminfo
-rm /usr/sbin/amule-*
-rm /etc/init.d/amule
 rm -r /usr/share/amule/
 
 sed -i '/--dport 4662 -j/d' /etc/firewall.user
