f719b2bb70834f5f1e46835c4a1d1bc234609c70
Francesco Molitierno <franc.molitierno@gmail.com>
2019-01-10 14:42:56 +0100

    move all dir to /tmp avoid replacing ipk files

--- setup.sh
+++ setup.sh
@@ -1,18 +1,11 @@
 #!/bin/sh
 
 opkg install ./*.ipk
-cp ./transmission-st* /usr/sbin/
-cp ./transmission /etc/init.d/
 cp ./transmission.conf /etc/config/transmission
-#cat <<EOF >> /etc/firewall.user
-#iptables -I INPUT  -p tcp -m tcp --dport 6800:6801 -j ACCEPT
-#iptables -I INPUT  -p udp -m udp --dport 6801 -j ACCEPT
-#EOF
 cp ./remove.sh /usr/share/transformer/scripts/remove_transmission.sh
-if [ -d /mnt/usb/USB-A1 ]; then
-    mkdir -p /mnt/usb/USB-A1/sharing/config/transmission/
-    mkdir -p /mnt/usb/USB-A1/sharing/download/transmission
-    cp -R ./config/* /mnt/usb/USB-A1/sharing/config/transmission/
-    ln -s /mnt/usb/USB-A1/sharing/config/transmission/ /usr/share/transmission
-    ln -s /mnt/usb/USB-A1/sharing/config/transmission/ /www/docroot/transmission
+if [ -d /tmp/run/mountd/sda1 ]; then
+    mkdir -p /tmp/run/mountd/sda1/sharing/config/transmission/
+    mkdir -p /tmp/run/mountd/sda1/sharing/download/transmission
+    cp -R ./config/* /tmp/run/mountd/sda1/sharing/config/transmission/
+    ln -s /tmp/run/mountd/sda1/sharing/config/transmission/ /usr/share/transmission
 fi
