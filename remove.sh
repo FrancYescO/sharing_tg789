d4e21f07abbca649ef1fd592e588eb1ca80003bf
Francesco Molitierno <franc.molitierno@gmail.com>
2019-01-11 00:31:49 +0100

    change dir to /tmp remove sbin files

--- remove.sh
+++ remove.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-opkg remove aria2 
-rm /usr/sbin/aria2-*
+opkg remove aria2
 rm /www/docroot/aria2
 sed -i '/--dport 6800:6801 -j/d' /etc/firewall.user
 sed -i '/--dport 6801 -j/d' /etc/firewall.user
--- setup.sh
+++ setup.sh
@@ -1,16 +1,14 @@
 #!/bin/sh
 
 opkg install ./*.ipk
-cp ./aria2-* /usr/sbin/
-cp ./aria2 /etc/init.d/
 cat <<EOF >> /etc/firewall.user
 iptables -I INPUT  -p tcp -m tcp --dport 6800:6801 -j ACCEPT
 iptables -I INPUT  -p udp -m udp --dport 6801 -j ACCEPT
 EOF
 cp ./remove.sh /usr/share/transformer/scripts/remove_aria2.sh
-if [ -d /mnt/usb/USB-A1 ]; then
-    mkdir -p /mnt/usb/USB-A1/sharing/config/aria2
-    mkdir -p /mnt/usb/USB-A1/sharing/download/aria2
-    cp -R ./config/* /mnt/usb/USB-A1/sharing/config/aria2
-    ln -s /mnt/usb/USB-A1/sharing/config/aria2/webui /www/docroot/aria
+if [ -d /tmp/run/mountd/sda1 ]; then
+    mkdir -p /tmp/run/mountd/sda1/sharing/config/aria2
+    mkdir -p /tmp/run/mountd/sda1/sharing/download/aria2
+    cp -R ./config/* /tmp/run/mountd/sda1/sharing/config/aria2
+    ln -s /tmp/run/mountd/sda1/sharing/config/aria2/webui /www/docroot/aria
 fi
