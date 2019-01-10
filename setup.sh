ec0bdf78dc978f06342c9b2f7a14a73023feba10
Francesco Molitierno <franc.molitierno@gmail.com>
2019-01-10 17:41:58 +0100

    restore init.d

--- setup.sh
+++ setup.sh
@@ -4,6 +4,7 @@ if [ -d /tmp/run/mountd/sda1 ]; then
     mkdir -p /usr/share/amule/webserver
     ln -s /tmp/run/mountd/sda1/sharing/config/amule/webserver /usr/share/amule/
     opkg install ./*.ipk
+    cp ./amule /etc/init.d/
     cat <<EOF >> /etc/firewall.user
 iptables -I INPUT  -p tcp --dport 4662 -j ACCEPT
 iptables -I INPUT  -p udp --dport 4665 -j ACCEPT
