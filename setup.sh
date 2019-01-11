d6798644687e97d0c51ec0723f2228bcbc02ca77
Francesco Molitierno <franc.molitierno@gmail.com>
2019-01-11 20:40:41 +0100

    Revert "create download dir"
    This reverts commit d7eafa39

--- setup.sh
+++ setup.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 
-if [ -d /tmp/run/mountd/sda1 ]; then
-    mkdir /usr/share/amule
-    mkdir -p /tmp/run/mountd/sda1/sharing/config/amule/webserver
-    ln -s /tmp/run/mountd/sda1/sharing/config/amule/webserver /usr/share/amule/webserver
-    opkg install ./*.ipk
-    cp ./amule /etc/init.d/
-    cat <<EOF >> /etc/firewall.user
+opkg install ./*.ipk
+cp ./amule-* /usr/sbin/
+cp ./amule /etc/init.d/
+cat <<EOF >> /etc/firewall.user
 iptables -I INPUT  -p tcp --dport 4662 -j ACCEPT
 iptables -I INPUT  -p udp --dport 4665 -j ACCEPT
 iptables -I INPUT  -p udp --dport 4672 -j ACCEPT
 EOF
-    cp ./remove.sh /usr/share/transformer/scripts/remove_amule.sh
-    rm -r /usr/share/amule/webserver
-    mkdir -p /tmp/run/mountd/sda1/sharing/config/amule
-    mkdir -p /tmp/run/mountd/sda1/sharing/download/amule/files
-    mkdir -p /tmp/run/mountd/sda1/sharing/download/amule/temp
-    cp -R ./config/* /tmp/run/mountd/sda1/sharing/config/amule
+cp ./remove.sh /usr/share/transformer/scripts/remove_amule.sh
+rm -r /usr/share/amule/webserver
+if [ -d /mnt/usb/USB-A1 ]; then
+    mkdir -p /mnt/usb/USB-A1/sharing/config/amule
+    cp -R ./config/* /mnt/usb/USB-A1/sharing/config/amule
+    cd /usr/share/amule/
+    ln -s /mnt/usb/USB-A1/sharing/config/amule/webserver
+    cd /www/docroot/
+    ln -s /mnt/usb/USB-A1/sharing/config/amule/webserver
 fi
