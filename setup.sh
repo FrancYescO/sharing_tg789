e60c7fb2935785c087332f1c9cb586589888846b
Francesco Molitierno <franc.molitierno@gmail.com>
2019-01-11 21:47:46 +0100

    restore UI-Reloaded move to tmp folder

--- setup.sh
+++ setup.sh
@@ -1,17 +1,18 @@
 #!/bin/sh
 
-opkg install ./*.ipk
-cp ./amule /etc/init.d/
-cat <<EOF >> /etc/firewall.user
+if [ -d /tmp/run/mountd/sda1 ]; then
+    mkdir /usr/share/amule
+    mkdir -p /tmp/run/mountd/sda1/sharing/config/amule/webserver
+    cp -R ./config/* /tmp/run/mountd/sda1/sharing/config/amule
+    ln -s /tmp/run/mountd/sda1/sharing/config/amule/webserver /usr/share/amule/
+    opkg install ./*.ipk
+    cp ./amule /etc/init.d/
+    cat <<EOF >> /etc/firewall.user
 iptables -I INPUT  -p tcp --dport 4662 -j ACCEPT
 iptables -I INPUT  -p udp --dport 4665 -j ACCEPT
 iptables -I INPUT  -p udp --dport 4672 -j ACCEPT
 EOF
-/etc/init.d/firewall reload
-cp ./remove.sh /usr/share/transformer/scripts/remove_amule.sh
-#rm -r /usr/share/amule/webserver
-if [ -d /mnt/usb/USB-A1 ]; then
-    mkdir -p /mnt/usb/USB-A1/sharing/config/amule
-    cp -R ./config/* /mnt/usb/USB-A1/sharing/config/amule
-    #ln -s /mnt/usb/USB-A1/sharing/config/amule/webserver /usr/share/amule/
+    /etc/init.d/firewall reload
+    cp ./remove.sh /usr/share/transformer/scripts/remove_amule.sh
+    mkdir -p /tmp/run/mountd/sda1/sharing/config/amule
 fi
