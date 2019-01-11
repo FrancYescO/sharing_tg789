336dcf8ec5f0d64c9ea67a4af12025be45919da3
Francesco Molitierno <franc.molitierno@gmail.com>
2019-01-11 21:03:28 +0100

    webserver folder from flash

--- setup.sh
+++ setup.sh
@@ -1,20 +1,17 @@
 #!/bin/sh
 
 opkg install ./*.ipk
-cp ./amule-* /usr/sbin/
 cp ./amule /etc/init.d/
 cat <<EOF >> /etc/firewall.user
 iptables -I INPUT  -p tcp --dport 4662 -j ACCEPT
 iptables -I INPUT  -p udp --dport 4665 -j ACCEPT
 iptables -I INPUT  -p udp --dport 4672 -j ACCEPT
 EOF
+/etc/init.d/firewall reload
 cp ./remove.sh /usr/share/transformer/scripts/remove_amule.sh
-rm -r /usr/share/amule/webserver
+#rm -r /usr/share/amule/webserver
 if [ -d /mnt/usb/USB-A1 ]; then
     mkdir -p /mnt/usb/USB-A1/sharing/config/amule
     cp -R ./config/* /mnt/usb/USB-A1/sharing/config/amule
-    cd /usr/share/amule/
-    ln -s /mnt/usb/USB-A1/sharing/config/amule/webserver
-    cd /www/docroot/
-    ln -s /mnt/usb/USB-A1/sharing/config/amule/webserver
+    #ln -s /mnt/usb/USB-A1/sharing/config/amule/webserver /usr/share/amule/
 fi
