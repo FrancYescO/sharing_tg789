368b0c4e9ae0d0f67d37f8af9cf5dc107b417581
Francesco Molitierno <franc.molitierno@gmail.com>
2019-01-12 10:11:13 +0100

    stop before unistall

--- remove.sh
+++ remove.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-rm /usr/share/transmission
+/etc/init.d/transmission stop
 opkg remove transmission-daemon-openssl transmission-remote-openssl ca-bundle libminiupnpc libnatpmp  libevent2 #--force-removal-of-dependent-packages
+rm /usr/share/transmission
 rm /etc/config/transmission
 
 
