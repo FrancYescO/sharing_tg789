ce1a06b0e5fc407e35bed59601e477501d3a9735
Francesco Molitierno <franc.molitierno@gmail.com>
2019-01-15 10:17:56 +0100

    asterisk-gui

--- remove.sh
+++ remove.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
-
-/etc/init.d/transmission stop
-opkg remove transmission-daemon-openssl transmission-remote-openssl ca-bundle libminiupnpc libnatpmp  libevent2 #--force-removal-of-dependent-packages
-rm /usr/share/transmission
-rm /etc/config/transmission
-
-
-
-
+/etc/init.d/asterisk stop
+/etc/init.d/asterisk disable
+/etc/init.d/asterisk-gui disable
+opkg remove asterisk13-app-exec
+opkg remove asterisk13-app-system
+opkg remove asterisk13-chan-iax2
+opkg remove asterisk13-res-timing-timerfd
+opkg remove asterisk13
+opkg remove libncurses
+opkg remove terminfo
+rm /bin/hostname
+rm -r /etc/asterisk
+rm -r /usr/share/asterisk
+rm -r /usr/lib/asterisk
+rm -r /var/lib/asterisk
--- setup.sh
+++ setup.sh
@@ -1,14 +1,25 @@
 #!/bin/sh
 
-opkg install ./*.ipk
-cp ./transmission.conf /etc/config/transmission
-cp ./remove.sh /usr/share/transformer/scripts/remove_transmission.sh
-if [ -d /tmp/run/mountd/sda1 ]; then
-    mkdir -p /tmp/run/mountd/sda1/sharing/config/transmission/
-    mkdir -p /tmp/run/mountd/sda1/sharing/download/transmission
-    cp -R ./config/* /tmp/run/mountd/sda1/sharing/config/transmission/
-    mkdir -p /tmp/run/mountd/sda1/sharing/config/transmission/config/torrents
-    mkdir -p /tmp/run/mountd/sda1/sharing/config/transmission/config/resume
-    mkdir -p /tmp/run/mountd/sda1/sharing/config/transmission/config/blocklists
-    ln -s /tmp/run/mountd/sda1/sharing/config/transmission/ /usr/share/transmission
-fi
+opkg install terminfo
+opkg install libncurses
+opkg install asterisk13
+opkg install asterisk13-res-timing-timerfd
+opkg install asterisk13-chan-iax2
+opkg install asterisk13-app-system
+opkg install asterisk13-app-exec
+cp -r asterisk-gui_data/* /
+cp *.conf /etc/asterisk/
+mkdir -p /usr/lib/asterisk/sounds/en
+cp demo-echotest.alaw /usr/lib/asterisk/sounds/en
+chmod -R 755 /etc/asterisk
+chmod -R 755 /usr/lib/asterisk
+#chmod 755 /etc/init.d/asterisk
+chmod 755 /etc/init.d/asterisk-gui
+chmod 755 /bin/hostname
+rm -R /var/lib/asterisk
+/etc/init.d/asterisk enable
+/etc/init.d/asterisk-gui enable
+/etc/init.d/asterisk-gui restart
+sudo ln -s /var/lib/asterisk/static-http/ /usr/share/asterisk/static-http
+/etc/init.d/asterisk restart
+start.sh
