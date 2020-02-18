9f937dd0789befda7c7a8c7212d1bba8676a91e8
Francesco Molitierno <franc.molitierno@gmail.com>
2020-02-18 20:57:53 +0100

    transmission-789xtream35b

--- remove.sh
+++ remove.sh
@@ -3,5 +3,6 @@
 /etc/init.d/aria2 stop
 opkg remove aria2
 rm /www/docroot/aria
+rm /etc/init.d/aria2
 sed -i '/--dport 6800:6801 -j/d' /etc/firewall.user
 sed -i '/--dport 6801 -j/d' /etc/firewall.user
