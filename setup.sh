2d1965ae4c34602ae0baa05e8bd24efa85e704d7
Francesco Molitierno <franc.molitierno@gmail.com>
2020-04-26 03:50:35 +0200

    align with new wiki instructions https://openwrt.org/docs/guide-user/services/vpn/ipsec/strongswan/roadwarrior

--- setup.sh
+++ setup.sh
@@ -4,11 +4,14 @@ echo "Installing strongswan..."
 
 opkg install strongswan-default strongswan-pki strongswan-mod-dhcp
 
-vpnclientName="myvpnclient";
-echo "Generated client cert, take it from /tmp/""$vpnclientName""Cert.p12 !!"
-orgName="Technicolor"
-caName="CATechnicolor"
-ddns_domain=$(uci get ddns.myddns_ipv4.domain)
+COUNTRYNAME="US"
+CANAME="CATechnicolor"
+ORGNAME="Technicolor"
+CACERTPASSWORD="" #if set will be asked when installing cert on clients or generating new clientCert
+SERVERDOMAINNAME=$(uci get ddns.myddns_ipv4.domain) #"myvpnserver.dyndns.org"
+CLIENTNAMES="myvpnclient1" # or more " … myvpnclient2 muvpnclient3"
+SHAREDSAN="myVpnClients" # iOS clients need to match a common SAN
+
 dhcp_broadcast=$(ifconfig br-lan | awk '/inet / {print $3}' | cut -d: -f2)
 
 echo "Generating/Placing conf files..."
@@ -16,20 +19,23 @@ echo "Generating/Placing conf files..."
 echo "config setup
 
 conn %default
- keyexchange=ikev2
+        keyexchange=ikev2
+        ike=aes256-aes128-sha256-sha1-modp3072-modp2048-modp1024
+        left=%any
+        leftauth=pubkey
+        leftcert=serverCert_$SERVERDOMAINNAME.pem
+        leftid=$SERVERDOMAINNAME
+        leftsubnet=0.0.0.0/0;::/0
+        right=%any
+        rightsourceip=10.0.1.0/24
+        #rightdns=8.8.8.8
+        eap_identity=%identity
+        auto=add
 
-conn roadwarrior
- left=%any
- leftauth=pubkey
- leftcert=serverCert.pem
- leftid=$ddns_domain
- leftsubnet=0.0.0.0/0,::/0
- right=%any
- rightsourceip=%dhcp
- rightauth=pubkey
- rightcert=clientCert.pem
- #rightauth2=eap-mschapv2
- auto=add" > /etc/ipsec.conf
+conn rwEAPMSCHAPV2
+        leftsendcert=always
+        rightauth=eap-mschapv2
+        rightsendcert=never" > /etc/ipsec.conf
 
 echo "dhcp {
   force_server_address = yes
@@ -37,10 +43,8 @@ echo "dhcp {
   server = $dhcp_broadcast
 }" > /etc/strongswan.d/charon/dhcp.conf
 
-cat << EOF > /etc/ipsec.secrets
-: RSA serverKey.pem
-remoteusername : EAP "secretpassword"
-EOF
+echo ": RSA serverKey_$SERVERDOMAINNAME.pem
+remoteusername : EAP \"secretpassword\"" > /etc/ipsec.secrets
 
 cat << EOF >> /etc/config/firewall
 
@@ -78,36 +82,86 @@ iptables -I INPUT  -m policy --dir in --pol ipsec --proto esp -j ACCEPT
 iptables -I FORWARD  -m policy --dir in --pol ipsec --proto esp -j ACCEPT
 iptables -I FORWARD  -m policy --dir out --pol ipsec --proto esp -j ACCEPT
 iptables -I OUTPUT   -m policy --dir out --pol ipsec --proto esp -j ACCEPT
+iptables -t nat -I POSTROUTING -m policy --pol ipsec --dir out -j ACCEPT
 
 EOF
 
 cd /tmp
 
-echo "Generating Keys/Certs, it requires some time..."
+echo "Building certificates for [ $SERVERDOMAINNAME ] and client [ $CLIENTNAME (aka $SHAREDSAN) ] "
 
-echo "Generating CA Key/Certificate..."
-ipsec pki --gen --outform pem > caKey.pem
-ipsec pki --self --in caKey.pem --dn "C=US, O=$orgName, CN=$caName" --ca --outform pem > caCert.pem
+[ -f "/etc/ipsec.d/private/ca.p12" ] && ln -s /etc/ipsec.d/private/ca.p12 ca.p12
 
-echo "Generating Server Key/Certificate..."
-ipsec pki --gen --outform pem > serverKey.pem
-ipsec pki --pub --in serverKey.pem | ipsec pki --issue --cacert caCert.pem --cakey caKey.pem --dn "C=US, O=$orgName, CN=$ddns_domain" --san="$ddns_domain" --flag serverAuth --flag ikeIntermediate --outform pem > serverCert.pem
+if [ -f "caKey.pem" ] ; then
+  echo "caKey exists, using existing caKey for signing serverCert and clientCert...."
+elif [ -f "ca.p12" ] ; then
+  echo "CA keys bundle exists, accessing existing protected caKey for signing serverCert and clientCert...."
+  openssl pkcs12 -in ca.p12 -nocerts -out caKey.pem
+else
+  echo "generating a new cakey for [ $CANAME ]"
+  ipsec pki --gen --outform pem > caKey.pem
+fi
+echo "generating caCert for [ $CANAME ]..."
+ipsec pki --self --lifetime 3652 --in caKey.pem --dn "C=$COUNTRYNAME, O=$ORGNAME, CN=$CANAME" --ca --outform pem > caCert.pem
+openssl x509 -inform PEM -outform DER -in caCert.pem -out caCert.crt
+echo "Now building CA keys bundle"
+openssl pkcs12 -export -inkey caKey.pem -in caCert.pem -name "$CANAME" -certfile caCert.pem -caname "$CANAME" -out ca.p12 -password "pass:$CACERTPASSWORD"
 
-echo "Generating Client Key/Certificate..."
-ipsec pki --gen --outform pem > clientKey.pem
-ipsec pki --pub --in clientKey.pem | ipsec pki --issue --cacert caCert.pem --cakey caKey.pem --dn "C=US, O=$orgName, CN=$vpnclientName" --san="$vpnclientName" --outform pem > clientCert.pem
+echo "generating server certificates for [ $SERVERDOMAINNAME ]... "
+ipsec pki --gen --outform pem > serverKey_$SERVERDOMAINNAME.pem
+ipsec pki --pub --in serverKey_$SERVERDOMAINNAME.pem | ipsec pki --issue --lifetime 3652 --cacert caCert.pem --cakey caKey.pem --dn "C=$COUNTRYNAME, O=$ORGNAME, CN=$SERVERDOMAINNAME" --san="$SERVERDOMAINNAME" --flag serverAuth --flag ikeIntermediate --outform pem > serverCert_$SERVERDOMAINNAME.pem
+#openssl x509 -inform PEM -outform DER -in serverCert_$SERVERDOMAINNAME.pem -out serverCert_$SERVERDOMAINNAME.crt
 
-openssl pkcs12 -export -inkey clientKey.pem -in clientCert.pem -name "$vpnclientName" -certfile caCert.pem -caname "$caName" -out "$vpnclientName""Cert.p12" -passout pass:
+for CLIENTNAME in $CLIENTNAMES; do
+  if [ -f "clientCert_$CLIENTNAME.pem" ] ; then
+    echo "clientCert for [ $CLIENTNAME ] exists, not generating new clientCert."
+    continue
+  fi
+  echo "generating clientCert for [ $CLIENTNAME (aka $SHAREDSAN) ]..."
+  ipsec pki --gen --outform pem > clientKey_$CLIENTNAME.pem
+  ipsec pki --pub --in clientKey_$CLIENTNAME.pem | ipsec pki --issue --lifetime 3652 --cacert caCert.pem --cakey caKey.pem --dn "C=$COUNTRYNAME, O=$ORGNAME, CN=$CLIENTNAME" --san="$CLIENTNAME" --san="$SHAREDSAN" --outform pem > clientCert_$CLIENTNAME.pem
+  openssl x509 -inform PEM -outform DER -in clientCert_$CLIENTNAME.pem -out clientCert_$CLIENTNAME.crt
+  echo "Now building Client keys bundle for [ $CLIENTNAME ]"
+  openssl pkcs12 -export -inkey clientKey_$CLIENTNAME.pem -in clientCert_$CLIENTNAME.pem -name "$CLIENTNAME" -certfile caCert.pem -caname "$CANAME" -out client_$CLIENTNAME.p12 -password "pass:$CACERTPASSWORD"
+  rm clientKey_$CLIENTNAME.pem
+  openssl x509 -inform PEM -outform DER -in clientCert_$CLIENTNAME.pem -out clientCert_$CLIENTNAME.crt
 
-echo "Take client Cert from /tmp/""$vpnclientName""Cert.p12 !!"
+  echo "
+conn rwPUBKEYIOS
+        leftsendcert=always
+        rightid=SHAREDSAN
+        rightauth=pubkey
+        rightcert=clientCert_$CLIENTNAME.pem
+        #rightauth2=eap-mschapv2
+
+conn rwEAPTLSIOS
+        leftsendcert=always
+        rightid=SHAREDSAN
+        rightauth=eap-tls
+        rightcert=clientCert_$CLIENTNAME.pem
+        #rightauth2=eap-mschapv2
+
+conn rwPUBKEY
+        rightauth=pubkey
+        rightcert=clientCert_$CLIENTNAME.pem
+        #rightauth2=eap-mschapv2
+
+conn rwEAPTLS
+        rightauth=eap-tls
+        rightcert=clientCert_$CLIENTNAME.pem
+" >> /etc/ipsec.conf
+done
 
 # where to put them...
 mv caCert.pem /etc/ipsec.d/cacerts/
-mv caKey.pem /etc/ipsec.d/private/
-mv serverCert.pem /etc/ipsec.d/certs/
-mv serverKey.pem /etc/ipsec.d/private/
-mv clientCert.pem /etc/ipsec.d/certs/
-mv clientKey.pem /etc/ipsec.d/private/
+mv serverCert*.pem /etc/ipsec.d/certs/
+mv serverKey*.pem /etc/ipsec.d/private/
+mv clientCert*.pem /etc/ipsec.d/certs/
+
+#These file are not needed on the modem
+[ ! -f "/etc/ipsec.d/private/ca.p12" ] && mv ca.p12 /etc/ipsec.d/private/ #needed to generate new clients
+mv client_*.p12 /etc/ipsec.d/private/
+mv clientCert_*.crt /etc/ipsec.d/private/
 
 /etc/init.d/firewall restart
 /etc/init.d/ipsec enable
